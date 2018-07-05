6bad67abd8a448d81f1d806033a25bc6a5d08e75
balaskoa <Jeno.Balasko@ericsson.com>
2018-07-05 14:29:24 +0200

    Copyright update to 2000-2018
    Signed-off-by: balaskoa <Jeno.Balasko@ericsson.com>

--- src/SQL_parser_.tab.c
+++ src/SQL_parser_.tab.c
@@ -73,7 +73,7 @@
 #line 2 "SQL_parser.y" /* yacc.c:339  */
 
 /******************************************************************************
-* Copyright (c) 2012, 2014  Ericsson AB
+* Copyright (c) 2000-2018 Ericsson Telecom AB
 * All rights reserved. This program and the accompanying materials
 * are made available under the terms of the Eclipse Public License v2.0
 * which accompanies this distribution, and is available at
--- src/lex.SQL_parser_.c
+++ src/lex.SQL_parser_.c
@@ -3137,7 +3137,7 @@ char *SQL_parser_text;
 #line 1 "SQL_parser.l"
 #line 3 "SQL_parser.l"
 /******************************************************************************
-* Copyright (c) 2012, 2014  Ericsson AB
+* Copyright (c) 2000-2018 Ericsson Telecom AB
 * All rights reserved. This program and the accompanying materials
 * are made available under the terms of the Eclipse Public License v2.0
 * which accompanies this distribution, and is available at
